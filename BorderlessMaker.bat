07cf93b6d210bf5758c9a2ae2f74e8f20d472dbe
RhythmLunatic <kirbyrulez@att.net>
2017-12-31 14:57:17 -0600

    Make it easier to use

--- BorderlessMaker.bat
+++ BorderlessMaker.bat
@@ -1,4 +1,10 @@
 @echo off
+
+::Set the executable name, the app width, and the app height here!
+SET applicationName=th145e.exe
+SET applicationWidth=1280
+SET applicationHeight=720
+
 ::This script requires nircmd and grep installed in your PATH along with BorderlessGaming (or equivalent)
 ::It matches the monitor resolution to the resolution of the game, then makes the game borderless, and
 ::after you close the game it will reset back to the maximum resolution of the display device.
@@ -6,8 +12,8 @@
 ::so if you need a fix, do it yourself.
 for /f "tokens=*" %%i in ('wmic desktopmonitor get screenheight  ^| grep -o "[[:digit:]]*"') do set DisplayHeight=%%i
 for /f "tokens=*" %%i in ('wmic desktopmonitor get screenwidth  ^| grep -o "[[:digit:]]*"') do set DisplayWidth=%%i
-nircmd setdisplay 1280 720 32
+nircmd setdisplay %applicationWidth% %applicationHeight% 32
 start "" "C:\Program Files (x86)\Borderless Gaming\BorderlessGaming.exe"
-th145e.exe
+%applicationName%
 taskkill /IM BorderlessGaming.exe /F
 nircmd setdisplay %DisplayWidth% %DisplayHeight% 32
