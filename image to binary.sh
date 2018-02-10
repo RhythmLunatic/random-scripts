e18a4b5adf684fe6f6c174c1704b72e6eaed6040
RhythmLunatic <kirbyrulez@att.net>
2018-02-10 03:06:02 -0600

    Image to binary converter!

---
+++ image to binary.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Trying to decode monika.chr from Doki Doki Literature Club? Use this!
+
+#Get width & height
+OUTPUT=$(convert $1 -print "%wx%h\n" /dev/null)
+#split into two variables (the character after IFS= is the char that it splits at)
+IFS=x read width height <<< $OUTPUT
+#Because seq ends at the number instead of stopping before it, subtract 1
+width=`expr $width - 1`
+height=`expr $height - 1`
+#Do our amazing loop
+for i in `seq 0 $height`;
+do
+	for j in `seq 0 $width`;
+	do
+    	#echo -n "$j x $i | "
+    	#Oh god why
+    	OUTPUT=$(convert $1 -crop "1x1+$j+$i" txt:- | awk 'END {print $NF}')
+    	[ "$OUTPUT" == "white" ] || ( echo -n 0 && false ) && echo -n "1"
+    	#echo ""
+    done
+done
+echo ""
